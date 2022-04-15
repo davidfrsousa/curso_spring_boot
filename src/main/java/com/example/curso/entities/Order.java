0eda353c9c8fdbb94b968ff4d3bc08537715d03c
tndg1 <davidfrsousa@hotmail.com>
2022-04-15 14:12:11 -0300

    Payment class

--- src/main/java/com/example/curso/entities/Order.java
+++ src/main/java/com/example/curso/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.example.curso.entities.enums.OrderStatus;
@@ -36,8 +38,10 @@ public class Order implements Serializable {
 	private User client;
 	private Integer orderStatus;
 	@OneToMany(mappedBy = "id.order")
-	private Set<OrderItem> items = new HashSet<>();	
-	
+	private Set<OrderItem> items = new HashSet<>();
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+
 	public Order() {
 	}
 
@@ -72,6 +76,14 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
@@ -80,8 +92,8 @@ public class Order implements Serializable {
 		if (orderStatus != null)
 			this.orderStatus = orderStatus.getCode();
 	}
-	
-	public Set<OrderItem> getItems(){
+
+	public Set<OrderItem> getItems() {
 		return items;
 	}
 
